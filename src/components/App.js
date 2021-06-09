c4aef9fe065b2e1146a2d0ad748ed96416cc7038
zpskek <wjdghdwns0@gmail.com>
2021-06-09 15:03:18 +0900

    Cleaning code

--- src/components/App.js
+++ src/components/App.js
@@ -14,10 +14,14 @@ function App() {
           uid: user.uid,
           updateProfile: (args) => user.updateProfile(args),
         });
+      } else {
+        setUserObj(null);
       }
       setInit(true);
     });
   }, []);
+
+  // 프로파일을 업데이트할 때 다시 랜더링이 될 수 있도록 refreshUser를 만든다.
   const refreshUser = () => {
     const user = authService.currentUser;
     setUserObj({
