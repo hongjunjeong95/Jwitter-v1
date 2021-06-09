eda2a8a59387fc511f68fb5391bea6a59283608a
zpskek <wjdghdwns0@gmail.com>
2021-06-09 15:33:00 +0900

    Set nweets order desc in createdAt

--- src/components/NweetFactory.js
+++ src/components/NweetFactory.js
@@ -3,6 +3,7 @@ import { v4 as uuidv4 } from "uuid";
 import { storageService, dbService } from "fbase";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus, faTimes } from "@fortawesome/free-solid-svg-icons";
+
 const NweetFactory = ({ userObj }) => {
   const [nweet, setNweet] = useState("");
   const [attachment, setAttachment] = useState("");
